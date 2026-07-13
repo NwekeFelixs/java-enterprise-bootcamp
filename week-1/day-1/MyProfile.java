27a73fcfd3f9ee5c3385a4f3295f6554c326f906
unknown <felixonyekwere30@gmail.com>
2026-07-13 05:28:59 +0100

    Day 1 learning Java

---
+++ week-1/day-1/MyProfile.java
@@ -0,0 +1,20 @@
+public class  MyProfile {
+    public static void main(String[] args) {
+        String name = "Felix";
+        String favouriteFood = "Garri and Egusi Soup";
+        String favouriteProgrammingLanguage = "Java";
+        String dreamCompany = "Google";
+        int yearsOfExperience = 5;
+        boolean isLearningJava = true;
+
+
+        System.out.println("====== MY PROFILE =======");
+        System.out.println("Hello, my name is " + name + ".");
+        System.out.println("My favourite food is " + favouriteFood + ".");
+        System.out.println("My favourite programming language is " + favouriteProgrammingLanguage + ".");
+        System.out.println("My dream company is " + dreamCompany + ".");
+        System.out.println("I have " + yearsOfExperience + " years of experience.");
+        System.out.println("Am I currently learning Java? " + isLearningJava + ".");
+        System.out.println("===========================================");
+    }
+}
